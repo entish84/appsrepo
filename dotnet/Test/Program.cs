7d3f951f5eb046e8ae34e73a29f5b92744774bb0
entish84 <entishthoughts@outlook.com>
2026-05-17 21:56:10 +0530

    New Project

---
+++ dotnet/Test/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.ReadLine();
+String? inp = Console.ReadLine();
+Console.WriteLine($"Hello {inp}");
+if (inp == "Test") {
+	Console.WriteLine("Passed");
+}
+else {
+	Console.WriteLine("Failed");
+}
+Console.ReadLine();
